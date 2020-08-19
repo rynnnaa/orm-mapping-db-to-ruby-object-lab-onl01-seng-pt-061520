b65b6ac4cf665b1be7f1a3846bfa3cc6081a9a6e
Ryna Truong <rynatruong@outlook.com>
2020-08-19 15:33:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.all_students_in_grade_9(grade)
+  def self.all_students_in_grade_9
     sql = <<-SQL 
     SELECT *
     FROM Students
